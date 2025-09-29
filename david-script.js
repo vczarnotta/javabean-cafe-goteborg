4d5755e8de6c2b68fb1ce7f9bc4d240c9fe44fc5
David Lindblom <david@Davids-MacBook-Pro.local>
2025-09-29 11:50:02 +0200

    added blue when clicking burger

--- david-script.js
+++ david-script.js
@@ -5,8 +5,10 @@ const hamburger = document.getElementById("hamburger")
 // get #menu element
 const menu = document.getElementById("menu");
 
+const page = document.body;
 
-// when hamburger is clicked, add/remove class "open"
+// when #hamburger is clicked, add/remove class "open" on id #menu
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("open");
+    page.classList.toggle("blue-background");
 });
